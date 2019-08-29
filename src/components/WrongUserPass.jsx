848ded8b0b59bfd243fbcc03f6410cfa8e22c87d
ricoantonio <ricoantonio33@hotmail.com>
2019-08-29 13:12:57 +0700

    Wrong User alert

--- src/components/WrongUserPass.jsx
+++ src/components/WrongUserPass.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class WrongUserPass extends Component{
     render(){
         return(
-            <div className="alert-danger p-2 m-1">
+            <div className="alert-danger py-3">
                 <div className="text-center">
                     <b>Incorrect </b>username or password
                 </div>
